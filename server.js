0abc44f0cd639f169c5f68cf0a19e966f5697c02
Alexander West <westalex@hooch.ecs.vuw.ac.nz>
2017-05-26 16:27:38 +1200

    Initialsing directories

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+ res.send('Hello World!');
+});
+
+app.listen(port, function () {
+ console.log('Example app listening on port 8080!');
+});
