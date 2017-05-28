06151275a682001f1bfc6b81d1f10502c5f14873
serasong <sarahsssong@gmail.com>
2017-05-28 15:04:01 +1200

    added basic rest api methods

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
+var bodyParser = require('body-parser');
 var app = express();
-
 var port = process.env.PORT || 8080;
+var path = require('path'); 
+var pg = require('pg');
+var connectionString = ""; //change this when deployed to heroku? or use postgres on lab machine
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+	 // Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	 // // Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	 // Request headers you wish to allow ,
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-AllowHeaders');
+	 // Pass to next layer of middleware
+	next();
+});
 
 app.get('/', function (req, res) {
  res.send('Hello World!');
@@ -12,4 +29,30 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
  console.log('Example app listening on port 8080!');
+});
+
+//need product table & customer table
+//product table schema - ID (primary key), item name, quantity, UID
+//customer table schema ... need to work this out
+
+//product table - list all products (product name + quantity)
+app.get('/all', function (req, res){ 
+
+});
+
+//product table - return a product by uid
+app.get('/item', function (req, res){ 
+
+});
+
+app.post('/buy', function (req, res) { 
+
+});
+
+app.put('/update', function (req, res) {
+	
+});
+
+app.delete('/delete', function (req, res) {
+	
 });
