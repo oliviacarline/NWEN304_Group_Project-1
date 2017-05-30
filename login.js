ea99e99fed6c8d50d46ed19a1cd1da47e761ec93
serasong <sarahsssong@gmail.com>
2017-05-30 21:08:39 +1200

    added form data to array

--- login.js
+++ login.js
@@ -1,9 +1,12 @@
 $(document).ready(function(e) {
 	$("form[name='newAccountForm']").submit(function() {
 	    // get all the inputs into an array.
-	    var $inputs = $('#myForm :input');
+	    var data = $("form[name='newAccountForm']").serializeArray().reduce(function(obj, item) {
+		    obj[item.name] = item.value;
+		    return obj;
+		}, {});
 
-	    alert("hi");
+	    alert(data['username']);
 
 	});
 
