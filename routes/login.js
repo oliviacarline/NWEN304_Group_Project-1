e8bddb070aff281c19ab54bd85986cda9bb4e648
Shanshan Song <songshan@the-taj.ecs.vuw.ac.nz>
2017-06-02 15:31:54 +1200

    changed db url

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var database = 'postgres://songshan:Chips@depot:5432/songshan_jdbc';
+var database = 'postgres://lzffldlqkadbir:3c1cc00bb2b3b7bce086033be0a66167c9bb87c835ef455e3b60ae38cdcd27f0@ec2-23-21-220-167.compute-1.amazonaws.com:5432/dbuuirtv8ccpbj';
 var pg = require('pg').native;
 
 app.post('/login', function (req, res) {
