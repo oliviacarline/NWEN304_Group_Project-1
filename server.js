47764d5ce41a2916584a0bb8a24a8804aafe1594
Olivia Carline <carlinoliv@sen-sen.ecs.vuw.ac.nz>
2017-06-26 20:07:13 +1200

    oauth

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var router = express.Router();
+
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -7,6 +9,7 @@ var port = process.env.PORT || 8080;
 var path = require('path');
 var pg = require('pg');
 var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
 var session = require('express-session');
 var connectionString = "postgres://lzffldlqkadbir:3c1cc00bb2b3b7bce086033be0a66167c9bb87c835ef455e3b60ae38cdcd27f0@ec2-23-21-220-167.compute-1.amazonaws.com:5432/dbuuirtv8ccpbj";
 
@@ -40,6 +43,34 @@ app.use(function (req, res, next) {
 	next();
 });
 
+passport.use(new GoogleStrategy({
+    'clientID' : '193412241831-kr4f03mftc0206mb8bie8p59bm4764rc.apps.googleusercontent.com',
+        'clientSecret' : 'IK6IEHkBZ6T4WNyGo0ZlsHnf',
+        'callbackURL' : 'https://nwen304groupseven.herokuapp.com/auth/google/callback'
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      console.log("Access Token: " + accessToken); 
+      return done(null, profile);
+    });
+  }
+));
+
+/* Passport JS Routes */
+router.get('/auth/google',
+  passport.authenticate('google', { scope: 
+  	[ 'https://www.googleapis.com/auth/plus.login' ] }
+));
+ 
+router.get('/auth/google/callback', 
+    passport.authenticate( 'google', { 
+        successRedirect: '/',
+        failureRedirect: '/'
+}));
+
+
+
 // required for passport
 app.use(session({
     secret: 'ilovescotchscotchyscotchscotch', // session secret
@@ -56,6 +87,7 @@ app.listen(port, function () {
  console.log('Example app listening on port 8080!');
 });
 
+module.exports = router;
 
 //make routes directory
 
