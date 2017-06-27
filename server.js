ac292456f8ab713347fa43e95fe6ba0a47cc277d
Shanshan Song <songshan@sen-sen.ecs.vuw.ac.nz>
2017-06-27 19:00:10 +1200

    debug

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs');
-
 app.get('/', function(req, res) {
         res.render('./views/index.ejs');
     });
