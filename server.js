56124440f9d3d76b8131a60121a06bb2c8c624d8
Alex <alexwest4567@outlook.com>
2017-06-28 14:23:59 +1200

    Added https, need to test

--- server.js
+++ server.js
@@ -27,10 +27,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, function () {
+/*Start https---------------------
+Testing out https - sourced mostly from link below
+https://stackoverflow.com/questions/5998694/how-to-create-an-https-server-in-node-js
+Need to generate privatekey.pem and certificate.pem using the following commands:
+openssl genrsa -out privatekey.pem 1024
+openssl req -new -key privatekey.pem -out certrequest.csr
+openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+*/
+
+var fs = require('fs');
+var http = require('http');
+
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
+
+var credentials = {
+  privateKey: privateKey,
+  certificate: certificate
+};
+
+//Change below to https.createServer(options, app).listen(.....
+
+//app.listen(port, function () {
+https.createServer(credentials, app).listen(port, function() {
  console.log('Example app listening on port 8080!');
 });
-
+/*End https ------------------------*/
 
 //make routes directory
 
@@ -40,4 +63,7 @@ app.listen(port, function () {
 //app.use('/login'. login)
 
 //in each .js file (e.g login.js), need to put var router = express.Router();
-module.exports = app;
+
+//Https Change
+//module.exports = app;
+module.exports = https;
